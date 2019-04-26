cdf832900aab4a918e8b2d168694cff230804587
Iván Herrera <biherrerap@unal.edu.co>
2019-04-26 05:12:56 -0500

    Se agregan funciones de zoomin y zoomout

--- Parallel/main.cpp
+++ Parallel/main.cpp
@@ -1,3 +1,5 @@
+/*
+
 #include <opencv2/opencv.hpp>
 #include <iostream>
 
@@ -54,4 +56,67 @@ int main(int argc, char** argv)
 //    destroyWindow(windowName); //destroy the created window
     
     return 0;
+}
+*/
+
+
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace cv;
+
+/// Global variables
+Mat src, dst, tmp;
+char const* window_name = "Pyramids Demo";
+
+
+/**
+ * @function main
+ */
+int main( int argc, char** argv )
+{
+  /// General instructions
+  printf( "\n Zoom In-Out demo  \n " );
+  printf( "------------------ \n" );
+  printf( " * [u] -> Zoom in  \n" );
+  printf( " * [d] -> Zoom out \n" );
+  printf( " * [ESC] -> Close program \n \n" );
+
+  /// Test image - Make sure it s divisible by 2^{n}
+  src = imread( "/home/brehynnermraz/Documentos/PARALELA/Parallel/Parallel/input.jpeg" );
+  if( !src.data )
+    { printf(" No data! -- Exiting the program \n");
+      return -1; }
+
+  tmp = src;
+  dst = tmp;
+
+  /// Create window
+  namedWindow( window_name, CV_WINDOW_AUTOSIZE );
+  imshow( window_name, dst );
+
+  /// Loop
+  while( true )
+  {
+    int c;
+    c = waitKey(10);
+
+    if( (char)c == 27 )
+      { break; }
+    if( (char)c == 'u' )
+      { pyrUp( tmp, dst, Size( tmp.cols*2, tmp.rows*2 ) );
+        printf( "** Zoom In: Image x 2 \n" );
+      }
+    else if( (char)c == 'd' )
+     { pyrDown( tmp, dst, Size( tmp.cols/2, tmp.rows/2 ) );
+       printf( "** Zoom Out: Image / 2 \n" );
+     }
+
+    imshow( window_name, dst );
+    tmp = dst;
+  }
+  return 0;
 }
