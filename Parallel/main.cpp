ae2874ac42ff20a19ff698829586c4fcede5ed77
Iván Herrera <biherrerap@unal.edu.co>
2019-04-26 09:11:30 -0500

    Implementación Blur

--- Parallel/main.cpp
+++ Parallel/main.cpp
@@ -1,11 +1,8 @@
 /*
-
 #include <opencv2/opencv.hpp>
 #include <iostream>
-
 using namespace cv;
 using namespace std;
-
 int main(int argc, char** argv)
 {
     // Read the image file
@@ -57,7 +54,7 @@ int main(int argc, char** argv)
     
     return 0;
 }
-*/
+
 
 
 #include "opencv2/imgproc/imgproc.hpp"
@@ -73,17 +70,19 @@ Mat src, dst, tmp;
 char const* window_name = "Pyramids Demo";
 
 
-/**
- * @function main
- */
+
 int main( int argc, char** argv )
 {
+
+
   /// General instructions
   printf( "\n Zoom In-Out demo  \n " );
   printf( "------------------ \n" );
   printf( " * [u] -> Zoom in  \n" );
   printf( " * [d] -> Zoom out \n" );
   printf( " * [ESC] -> Close program \n \n" );
+
+
 
   /// Test image - Make sure it s divisible by 2^{n}
   src = imread( "/home/brehynnermraz/Documentos/PARALELA/Parallel/Parallel/input.jpeg" );
@@ -97,6 +96,14 @@ int main( int argc, char** argv )
   /// Create window
   namedWindow( window_name, CV_WINDOW_AUTOSIZE );
   imshow( window_name, dst );
+
+  pyrDown( tmp, dst, Size( tmp.cols/2, tmp.rows/2 ) );
+  pyrUp( tmp, dst, Size( tmp.cols*2, tmp.rows*2 ));
+
+  
+  imshow( window_name, dst );
+  tmp = dst;
+/*
 
   /// Loop
   while( true )
@@ -118,5 +125,140 @@ int main( int argc, char** argv )
     imshow( window_name, dst );
     tmp = dst;
   }
+
+
+
   return 0;
+}
+*/
+
+#include <iostream>
+#include <vector>
+#include <assert.h>
+#include <cmath>
+#include <png++/png.hpp>
+
+using namespace std;
+
+typedef vector<double> Array;
+typedef vector<Array> Matrix;
+typedef vector<Matrix> Image;
+
+//calcular el número gaussiano para multiplicar por la matriz de pixeles "kernel"
+
+Matrix getGaussian(int height, int width, double sigma)
+{
+    Matrix kernel(height, Array(width));
+    double sum=0.0;
+    int i,j;
+
+    for (i=0 ; i<height ; i++) {
+        for (j=0 ; j<width ; j++) {
+            kernel[i][j] = exp(-(i*i+j*j)/(2*sigma*sigma))/(2*M_PI*sigma*sigma);
+            sum += kernel[i][j];
+        }
+    }
+
+    for (i=0 ; i<height ; i++) {
+        for (j=0 ; j<width ; j++) {
+            kernel[i][j] /= sum;
+        }
+    }
+
+    return kernel;
+}
+
+//cargar imagen
+
+Image loadImage(const char *filename)
+{
+    png::image<png::rgb_pixel> image(filename);
+    Image imageMatrix(3, Matrix(image.get_height(), Array(image.get_width())));
+
+    int h,w;
+    for (h=0 ; h<image.get_height() ; h++) {
+        for (w=0 ; w<image.get_width() ; w++) {
+            imageMatrix[0][h][w] = image[h][w].red;
+            imageMatrix[1][h][w] = image[h][w].green;
+            imageMatrix[2][h][w] = image[h][w].blue;
+        }
+    }
+
+    return imageMatrix;
+}
+
+//guardar imagen 
+void saveImage(Image &image, const char *filename)
+{
+    assert(image.size()==3);
+
+    int height = image[0].size();
+    int width = image[0][0].size();
+    int x,y;
+
+    png::image<png::rgb_pixel> imageFile(width, height);
+
+    for (y=0 ; y<height ; y++) {
+        for (x=0 ; x<width ; x++) {
+            imageFile[y][x].red = image[0][y][x];
+            imageFile[y][x].green = image[1][y][x];
+            imageFile[y][x].blue = image[2][y][x];
+        }
+    }
+    imageFile.write(filename);
+}
+
+//función aplicar filtro blur
+Image applyFilter(Image &image, Matrix &filter){
+    assert(image.size()==3 && filter.size()!=0);
+
+    int height = image[0].size();
+    int width = image[0][0].size();
+    int filterHeight = filter.size();
+    int filterWidth = filter[0].size();
+    int newImageHeight = height-filterHeight+1;
+    int newImageWidth = width-filterWidth+1;
+    int d,i,j,h,w;
+
+    Image newImage(3, Matrix(newImageHeight, Array(newImageWidth)));
+
+    for (d=0 ; d<3 ; d++) {
+        for (i=0 ; i<newImageHeight ; i++) {
+            for (j=0 ; j<newImageWidth ; j++) {
+                for (h=i ; h<i+filterHeight ; h++) {
+                    for (w=j ; w<j+filterWidth ; w++) {
+                        newImage[d][i][j] += filter[h-i][w-j]*image[d][h][w];
+                    }
+                }
+            }
+        }
+    }
+
+    return newImage;
+}
+
+//aplicar filtro y generar imagen nueva
+
+Image applyFilter(Image &image, Matrix &filter, int times)
+{
+    Image newImage = image;
+    for(int i=0 ; i<times ; i++) {
+        newImage = applyFilter(newImage, filter);
+    }
+    return newImage;
+}
+
+
+
+int main()
+{
+    Matrix filter = getGaussian(5, 5, 10.0);
+
+    cout << "Loading image..." << endl;
+    Image image = loadImage("/home/brehynnermraz/Documentos/PARALELA/Parallel/Parallel/input.jpeg");
+    cout << "Applying filter..." << endl;
+    Image newImage = applyFilter(image, filter);
+    cout << "Saving image..." << endl;
+    saveImage(newImage, "newImage.png");
+    cout << "Done!" << endl;
 }
